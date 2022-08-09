602200c2c2ca6a028b5f9b72baecbb2bfed2699c
HiCoders <hicodersofficial@gmail.com>
2022-08-09 22:57:08 +0530

    🌱 Add simple like animation

---
+++ simple-like-animation/main.js
@@ -0,0 +1,11 @@
+const reactBtn = document.querySelector(".reaction");
+  let liked = false;
+  reactBtn.addEventListener("click", () => {
+    if (!liked) {
+      reactBtn.innerHTML = `<span class="like">❤️</span>`;
+      liked = true;
+    } else {
+      reactBtn.innerHTML = `<span class="unlike">🤍</span>`;
+      liked = false;
+    }
+  });
